d48bec949ed4e2c5e3179641f090497c8c2e2029
dskryl <d.skryl@perenio.com>
2022-05-17 19:41:33 +0300

    wiremock project init

---
+++ wiremock-tests/README.md
@@ -0,0 +1,3 @@
+# Using Wiremock to mock 3 party REST API calls
+
+> Mock API response from AUT server
---
+++ wiremock-tests/src/test/java/com/skryl/edu/MainTest.java
@@ -0,0 +1,74 @@
+package com.skryl.edu;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+/**
+ * @author Skryl D.V. on 2022-05-17
+ * Copyright © 2022 Perenio. All rights reserved.
+ */
+class MainTest {
+    private static WireMockServer wireMockServer;
+
+    @BeforeSuite
+    public static void setUp() {
+        wireMockServer = new WireMockServer(options().port(3001)); //No-args constructor will start on port 8080, no HTTPS
+        wireMockServer.start();
+
+        wireMockServer.stubFor(get("/api/books")
+                .willReturn(
+                        aResponse()
+                                .withStatus(200)
+                                .withStatusMessage("OK")
+                                .withBody("""
+                                        {
+                                          "content": [
+                                            {
+                                              "category": {
+                                                "id": 0,
+                                                "name": "string"
+                                              },
+                                              "categoryId": 0,
+                                              "format": {
+                                                "id": 0,
+                                                "name": "string"
+                                              },
+                                              "formatId": 0,
+                                              "id": 0,
+                                              "isbn": "string",
+                                              "title": "string"
+                                            }
+                                          ],
+                                          "last": true,
+                                          "numberOfElements": 0,
+                                          "page": 0,
+                                          "size": 0,
+                                          "totalElements": 0,
+                                          "totalPages": 0
+                                        }
+                                        """)
+                ));
+    }
+
+    @AfterSuite
+    public static void tearDown() {
+        wireMockServer.stop();
+    }
+
+
+    @Test
+    void getBooks() {
+        var response= RestAssured.given()
+                .accept(ContentType.JSON)
+                .get("http://localhost:3001/api/books");
+        response.getBody().prettyPrint();
+        response.then().statusCode(200);
+    }
+}
