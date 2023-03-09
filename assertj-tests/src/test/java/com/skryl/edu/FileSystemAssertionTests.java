92aa94c54b2367c51c71faf22c2f041f44443926
Dmitry Skryl <d.skryl@perenio.com>
2023-03-09 14:29:27 +0200

    add does not exit folder recursively

--- assertj-tests/src/test/java/com/skryl/edu/FileSystemAssertionTests.java
+++ assertj-tests/src/test/java/com/skryl/edu/FileSystemAssertionTests.java
@@ -3,7 +3,13 @@ package com.skryl.edu;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,6 +48,15 @@ public class FileSystemAssertionTests {
     }
 
     @Test
+    void verifyThatChildDirectoryExistInRootDirectoryRecursively() {
+        var folder = new File("src");
+        assertThat(folder)
+                .isDirectoryRecursivelyContaining(p -> p.getName().equals("edu"));
+        assertThat(folder)
+                .isDirectoryRecursivelyContaining("glob:**test*");
+    }
+
+    @Test
     void checkThatAssertJActuallyVerifyFolderRecursively() {
         var folder = new File("src");
         assertThat(folder)
@@ -53,6 +68,25 @@ public class FileSystemAssertionTests {
         var folder = new File("src");
         assertThat(folder)
                 .isDirectoryNotContaining("glob:**unexistdir*");
+    }
+
+    /**
+     * Should fail to verify assertion is correct
+     */
+    @Test
+    void verifyThatChildDirectoryDoesNotExistInRootRecursively() throws IOException {
+        var paths = listOfPaths("src");
+        assertThat(paths)
+                .map(Path::toFile)
+                .map(File::getName)
+                .doesNotContain("edu");
+    }
+
+    private List<Path> listOfPaths(String path) throws IOException {
+        try (Stream<Path> stream = Files.walk(Paths.get(path))) {
+            return stream.filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
+        }
     }
 
 }
