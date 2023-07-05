5d7d53b2005fa7b873d0f298bff2236fef59abad
Dmitry Skryl <d.skryl@perenio.com>
2023-07-05 17:59:07 +0300

    add chain capabilities

--- testng-tests/src/main/java/com/skryl/edu/User.java
+++ testng-tests/src/main/java/com/skryl/edu/User.java
@@ -1,11 +1,13 @@
 package com.skryl.edu;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 /**
  * @author Skryl D.V. on 2023-03-14
  */
 @Data
+@Accessors(chain = true)
 public class User {
     private String name;
     private int age;
